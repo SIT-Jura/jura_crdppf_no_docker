64357f8392911d717c9b6a09c780c5e2d1a1ae71
Raphael Wunderlich <raphael.wunderlich@jura.ch>
2021-12-13 08:23:15 +0100

    Update oereb-client

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 requires = [
     'pyramid_oereb[recommend]==2.0.0b6',
      #'oereb-client==1.3.8'
-     'oereb-client==2.0.0.dev202112090834'
+     'oereb-client==2.0.0b1.dev202112091016'
 ]
 
 setup(
