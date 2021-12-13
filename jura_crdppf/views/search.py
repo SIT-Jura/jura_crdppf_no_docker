d67a93771552b96be32e338c7166e8319d64cc44
Wolfgang Kaltz <wolfgang.kaltz@camptocamp.com>
2021-12-13 13:21:01 +0100

    Add Jura egrid search

---
+++ jura_crdppf/views/search.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import grequests
+import pkg_resources
+
+from defusedxml.ElementTree import fromstring
+from mako.template import Template
+
+
+def hook_egrid(config, response, lang, default_lang):
+    if len(response.get('features')) < 1:
+        return None
+
+    results = []
+
+    for result in response.get('features'):
+        egrid = result.get('properties').get('label')[0:-8]
+        results.append({
+            'label': egrid,
+            'egrid': egrid
+        })
+
+    return results
